7fe7fc5aa0e5eeca656f5294ab73a90ef30ca869
scooter7 <scooter.vineburgh@gmail.com>
2024-03-20 10:00:35 -0500

    Create app2.py

---
+++ app2.py
@@ -0,0 +1,86 @@
+import os
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import streamlit as st
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+os.getenv("GOOGLE_API_KEY")
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+def get_pdf_text_from_folder(folder_path):
+    text = ""
+    for file_name in os.listdir(folder_path):
+        if file_name.endswith('.pdf'):
+            pdf_path = os.path.join(folder_path, file_name)
+            pdf_reader = PdfReader(pdf_path)
+            for page in pdf_reader.pages:
+                text += page.extract_text() if page.extract_text() else ""
+    return text
+
+def get_text_chunks(text):
+    splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = splitter.split_text(text)
+    return chunks
+
+def get_vector_store(chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
+    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
+    Context:\n {context}?\n
+    Question: \n{question}\n
+
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro", client=genai, temperature=0.3)
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(llm=model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    print(response)
+    return response
+
+def main():
+    st.set_page_config(page_title="Gemini PDF Chatbot", page_icon="🤖")
+    pdf_folder_path = 'docs'
+    raw_text = get_pdf_text_from_folder(pdf_folder_path)
+    text_chunks = get_text_chunks(raw_text)
+    get_vector_store(text_chunks)
+
+    st.title("Chat with PDF files using Gemini🤖")
+    st.write("Welcome to the chat!")
+    if "messages" not in st.session_state:
+        st.session_state['messages'] = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
+
+    if prompt := st.chat_input():
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        response = user_input(prompt)
+        if response is not None:
+            full_response = ''.join(response['output_text'])
+            st.session_state.messages.append({"role": "assistant", "content": full_response})
+            with st.chat_message("assistant"):
+                st.markdown(full_response)
+
+if __name__ == "__main__":
+    main()
