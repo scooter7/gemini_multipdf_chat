c71bb7af3d76c8a0aa149134bd87b12d0a26dd43
scooter7 <scooter.vineburgh@gmail.com>
2024-04-24 12:17:28 -0500

    Update app2.py

--- app2.py
+++ app2.py
@@ -1,10 +1,12 @@
 import os
+import io
+import requests
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import streamlit as st
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS  # Updated import
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -34,7 +36,7 @@ def process_pdf_folder(folder_path):
     raw_text = get_pdf_text_from_folder(folder_path)
     text_chunks = get_text_chunks(raw_text)
     embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)  # Corrected argument
+    vector_store = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     vector_store.save_local("faiss_index")
     return vector_store
 
@@ -44,7 +46,6 @@ def get_conversational_chain():
     provided context just say, 'answer is not available in the context', don't provide the wrong answer\n\n
     Context:\n {context}?\n
     Question: \n{question}\n
-
     Answer:
     """
     model = ChatGoogleGenerativeAI(model="gemini-pro", client=genai, temperature=0.3)
@@ -56,7 +57,7 @@ def user_input(user_question, vector_store):
     new_db = FAISS.load_local("faiss_index", vector_store.embeddings, allow_dangerous_deserialization=True)
     docs = new_db.similarity_search(user_question)
     chain = get_conversational_chain()
-    response = chain({"input_documents": docs, "question": user_question}, return_only_outputs=True)
+    response = chain.invoke({"input_documents": docs, "question": user_question}, return_only_outputs=True)  # Use invoke here if necessary
     return response
 
 def main():
